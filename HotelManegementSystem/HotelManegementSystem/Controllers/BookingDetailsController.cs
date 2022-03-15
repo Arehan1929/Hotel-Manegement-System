d3fde28344bdfe80b2c39680dcb69a74e3fcfc19
Arehan1929 <rehan1929@users.noreply.github.com>
2022-03-15 15:43:33 +0530

    Initial project setup

---
+++ HotelManegementSystem/HotelManegementSystem/Controllers/BookingDetailsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelManegementSystem.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookingDetailsController : ControllerBase
+    {
+    }
+}
